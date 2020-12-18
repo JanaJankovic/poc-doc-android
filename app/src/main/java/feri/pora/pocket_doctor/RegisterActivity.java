3fcd0e7ee6973bc941c27f583eba1c3b25239419
jana_j <jana.jankovic@student.um.si>
2020-12-18 22:46:00 +0100

    dodan register, zaenkrat nic ni funkcionalno

---
+++ app/src/main/java/feri/pora/pocket_doctor/RegisterActivity.java
@@ -0,0 +1,14 @@
+package feri.pora.pocket_doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
