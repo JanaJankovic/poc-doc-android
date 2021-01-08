1b6810d40f5ed5d0831f00aa6316ecfaea90b8e8
jana_j <jana.jankovic@student.um.si>
2021-01-08 14:13:16 +0100

    bluetooth layout

--- app/src/main/java/feri/pora/pocket_doctor/fragments/OxymeterFragment.java
+++ app/src/main/java/feri/pora/pocket_doctor/fragments/OxymeterFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import feri.pora.pocket_doctor.R;
+import feri.pora.pocket_doctor.activities.UserNavigationActivity;
 
 public class OxymeterFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -19,6 +20,7 @@ public class OxymeterFragment extends Fragment {
         final ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_oxymeter, null);
 
+        ((UserNavigationActivity) requireActivity()).getSupportActionBar().hide();
         return  rootView;
     }
 }
