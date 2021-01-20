384d30ba902f07d1d4c4e865c84833e2e055fb96
jana_j <jana.jankovic@student.um.si>
2021-01-20 09:35:02 +0100

    podpora za backend

--- datalib/src/main/java/feri/pora/datalib/Prediction.java
+++ datalib/src/main/java/feri/pora/datalib/Prediction.java
@@ -7,12 +7,21 @@ public class Prediction {
     private String filePath;
     private String prediction;
     private String date;
+    private String base64;
 
     public Prediction(String filePath, String prediction, String date) {
         this.filePath = filePath;
         this.prediction = prediction;
         this.date = date;
         id = "";
+    }
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public void setBase64(String base64) {
+        this.base64 = base64;
     }
 
     public String getId() {
