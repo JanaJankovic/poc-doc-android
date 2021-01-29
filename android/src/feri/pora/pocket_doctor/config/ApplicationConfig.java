859f4a49cd448e9a3be5ac3c84f79a42fcc16b78
jana_j <jana.jankovic@student.um.si>
2021-01-29 14:21:21 +0100

    ip changes update

--- android/src/feri/pora/pocket_doctor/config/ApplicationConfig.java
+++ android/src/feri/pora/pocket_doctor/config/ApplicationConfig.java
@@ -3,7 +3,7 @@ package feri.pora.pocket_doctor.config;
 public class ApplicationConfig {
     public static final String USER_KEY = "user";
     //ipconfig -> wireless lan adapter ip4
-    public static final String HOST_IP_NODE = "http://192.168.0.105:3000";
-    public static final String HOST_IP_PYTHON = "http://192.168.0.103:9090";
-    public static final String BLOCKCHAIN_IP = "http://92.242.251.240:3001";
+    public static final String HOST_IP_NODE = "xxxxxx:3000"; // set up your own node ip
+    public static final String HOST_IP_PYTHON = "xxxxxxx:9090"; // set up your own python server ip
+    public static final String BLOCKCHAIN_IP = "xxxxxxx:3001"; //set up your own blockchain ip
 }
